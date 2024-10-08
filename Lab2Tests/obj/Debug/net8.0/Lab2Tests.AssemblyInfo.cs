775300b757bcde2360f84da4e696283d2cc5a61a
raiden-62 <afedin000@gmail.com>
2024-10-08 23:37:07 +0300

    I did not in fast complete Part 2, 2_12 and 2_13 still in the workshop

--- Lab2Tests/obj/Debug/net8.0/Lab2Tests.AssemblyInfo.cs
+++ Lab2Tests/obj/Debug/net8.0/Lab2Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+631ac352917fd3ad6caa3e8b45a75b750c3ff749")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff99dfa6e69bb4b0a4b13890c4079c9a6866ddd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
