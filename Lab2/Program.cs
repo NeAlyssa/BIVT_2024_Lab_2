7b27ac0b79331ccba188eb9a28b66035cbc39a8d
IFraimG <kulsasha55@gmail.com>
2024-10-18 16:49:49 +0300

    update tests 1,2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.Metrics;
+using System.Globalization;
 using System.Runtime.InteropServices;
 
 public class Program
@@ -10,31 +11,31 @@ public class Program
     public static void Main()
     {
         Program program = new Program();
-        //program.Task_1_1(0.9, 1.23);
-        //program.Task_1_2(0.9, 1.23);
-        //program.Task_1_3(0.9, 1.23);
-        //program.Task_1_4(0.9, 1.23, -0.1);
-        //program.Task_1_5(0.9, 1.23);
-        //program.Task_1_6(0.9, 1.23);
-        //program.Task_1_7(0.9);
-        //program.Task_1_8(0.9);
-        //program.Task_1_9(0.9);
-        //program.Task_1_10(0.9);
-        //program.Task_2_1(10);
-        //program.Task_2_2(5, 3, 2, 1);
-        //program.Task_2_2(5, 1.5, 1.5, 1);
-        //program.Task_2_2(5, 1, 3, 1);
-        //program.Task_2_3(10);
-        //program.Task_2_4(5, 1, 2);
-        //program.Task_2_5(10, 30);
-        //program.Task_2_6(5);
-        //program.Task_2_7(5);
-        //program.Task_2_8(5);
-        //program.Task_2_9(10);
-        //program.Task_2_10(10);
-        //program.Task_2_11(10);
-        //program.Task_2_12(10, 0);
-        //program.Task_2_13(10, 5, 0);
+        // program.Task_1_1(0.9, 1.23);
+        // program.Task_1_2(0.9, 1.23);
+        // program.Task_1_3(0.9, 1.23);
+        // program.Task_1_4(0.9, 1.23, -0.1);
+        // program.Task_1_5(0.9, 1.23);
+        // program.Task_1_6(0.9, 1.23);
+        // program.Task_1_7(0.9);
+        // program.Task_1_8(0.9);
+        // program.Task_1_9(0.9);
+        // program.Task_1_10(0.9);
+        // program.Task_2_1(10);
+        // program.Task_2_2(5, 3, 2, 1);
+        // program.Task_2_2(5, 1.5, 1.5, 1);
+        // program.Task_2_2(5, 1, 3, 1);
+        // program.Task_2_3(10);
+        // program.Task_2_4(3, 1, 3);
+        // program.Task_2_5(10, 30);
+        // program.Task_2_6(5);
+        // program.Task_2_7(5);
+        // program.Task_2_8(4);
+        // program.Task_2_9(10);
+        // program.Task_2_10(10);
+        // program.Task_2_11(10);
+        // program.Task_2_12(-0.75, 0);
+        program.Task_2_13(-0.75, 1, 0);
         //program.Task_3_1();
         //program.Task_3_2(3, 2, 1);
         //program.Task_3_2(1.5, 1.5, 1);
@@ -57,7 +58,13 @@ public class Program
         bool answer = false;
 
         // code here
+        double r = 2;
+        // (double, double) first = (0, 2);
+        // (double, double) second = (1.5, 0.7);
+        // (double, double) third = (1, 1);
+        // (double, double) fourth = (3, 0);
 
+        if (x * x + y * y == r * r && Math.Abs(x * x + y * y - r * r) <= Math.Pow(10, -3)) answer = true;
         // end
 
         return answer;
@@ -67,7 +74,7 @@ public class Program
         bool answer = false;
 
         // code here
-
+        if (y >= 0 && y + Math.Abs(x) <= 1.0) answer = true;
         // end
 
         return answer;
@@ -77,7 +84,13 @@ public class Program
         double answer = 0;
 
         // code here
+        double c = 0;
+        double maxFoo = a > b ? a : b;
+        double minFoo = a < b ? a : b;
+        if (a > 0) c = maxFoo;
+        else c = minFoo;
 
+        answer = c;
         // end
 
         return answer;
@@ -87,7 +100,10 @@ public class Program
         double answer = 0;
 
         // code here
+        double minFoo = a < b ? a : b;
+        double maxFoo = minFoo > c ? minFoo : c;
 
+        answer = maxFoo;
         // end
 
         return answer;
@@ -97,7 +113,7 @@ public class Program
         bool answer = false;
 
         // code here
-
+        if (2 * r >= s) answer = true;
         // end
 
         return answer;
@@ -107,7 +123,7 @@ public class Program
         bool answer = false;
 
         // code here
-
+        if (2 * r < s) answer = true;
         // end
 
         return answer;
@@ -117,7 +133,11 @@ public class Program
         double answer = 0;
 
         // code here
+        double y = 0;
+        if (Math.Abs(x) > 1) y = 1;
+        else y = Math.Abs(x);
 
+        answer = y;
         // end
 
         return answer;
@@ -127,7 +147,11 @@ public class Program
         double answer = 0;
 
         // code here
+        double y = 0;
+        if (Math.Abs(x) >= 1) y = 0;
+        else y = x * x - 1;
 
+        answer = y;
         // end
 
         return answer;
@@ -137,7 +161,11 @@ public class Program
         double answer = 0;
 
         // code here
-
+        double y = 0;
+        if (x <= -1) y = 0;
+        else if (-1 <= x && x <= 0) y = x + 1;
+        else y = 1;
+        answer = y;
         // end
 
         return answer;
@@ -147,7 +175,12 @@ public class Program
         double answer = 0;
 
         // code here
-
+        double y = 0;
+        if (x <= -1) y = 1;
+        else if (-1 < x && x <= 1) y = -1.0 * x;
+        else if (x > 1) y = -1;
+        
+        answer = y;
         // end
 
         return answer;
@@ -160,7 +193,15 @@ public class Program
         double answer = 0;
 
         // code here
+        double ages = 0;
+        for (int i = 0; i < n; i++)
+        {
+            string result = Console.ReadLine();
+            ages += double.Parse(result, new CultureInfo("en-US"));
+        }
 
+        answer = ages / n;
+        Console.WriteLine(answer);
         // end
 
         return answer;
@@ -170,7 +211,20 @@ public class Program
         int answer = 0;
 
         // code here
-
+        for (int i = 0; i < n; i++)
+        {
+            string result = Console.ReadLine();
+            string result2 = Console.ReadLine();
+            double x = double.Parse(result, new CultureInfo("en-US"));
+            double y = double.Parse(result2, new CultureInfo("en-US"));
+            
+            if ((x - a) * (x - a) + (y - b) * (y - b) <= r * r)
+            {
+                Console.WriteLine("______ {0}", Math.Pow(x - a, 2) + Math.Pow(y - b, 2) - Math.Pow(r, 2));
+                answer++;
+            }
+        }
+        Console.WriteLine(answer);
         // end
 
         return answer;
@@ -180,7 +234,17 @@ public class Program
         double answer = 0;
 
         // code here
-
+        for (int i = 0; i < n; i++)
+        {
+            string result = Console.ReadLine();
+            double foo = double.Parse(result, new CultureInfo("en-US"));
+            
+            if (foo < 30)
+            {
+                answer += 0.2;
+            }
+        }
+        Console.WriteLine(answer);
         // end
 
         return answer;
@@ -190,7 +254,19 @@ public class Program
         int answer = 0;
 
         // code here
-
+        for (int i = 0; i < n; i++)
+        {
+            string result = Console.ReadLine();
+            string result2 = Console.ReadLine();
+            double x = double.Parse(result, new CultureInfo("en-US"));
+            double y = double.Parse(result2, new CultureInfo("en-US"));
+            
+            if (x * x + y * y > r1 * r1 && x * x + y * y < r2 * r2)
+            {
+                answer++;
+            }
+        }
+        Console.WriteLine(answer);
         // end
 
         return answer;
@@ -200,7 +276,17 @@ public class Program
         int answer = 0;
 
         // code here
-
+        for (int i = 0; i < n; i++)
+        {
+            string result = Console.ReadLine();
+            double foo = double.Parse(result, new CultureInfo("en-US"));
+            
+            if (foo >= norm)
+            {
+                answer++;
+            }
+        }
+        Console.WriteLine(answer);
         // end
 
         return answer;
@@ -210,7 +296,19 @@ public class Program
         int answer = 0;
 
         // code here
+        for (int i = 0; i < n; i++)
+        {
+            string result = Console.ReadLine();
+            string result2 = Console.ReadLine();
+            double x = double.Parse(result, new CultureInfo("en-US"));
+            double y = double.Parse(result2, new CultureInfo("en-US"));
 
+            if ((x >= 0) && (x <= Math.PI) && (Math.Sin(x) >= y))
+            {
+                answer++;
+            }
+        }
+        Console.WriteLine(answer);
         // end
 
         return answer;
@@ -221,7 +319,23 @@ public class Program
         int answer3 = 0;
 
         // code here
+        for (int i = 0; i < n; i++)
+        {
+            string result = Console.ReadLine();
+            string result2 = Console.ReadLine();
+            double x = double.Parse(result, new CultureInfo("en-US"));
+            double y = double.Parse(result2, new CultureInfo("en-US"));
 
+            if (x >= 0 && y >= 0)
+            {
+                answer1++;
+            } else if (x < 0 && y < 0)
+            {
+                answer3++;
+            }
+        }
+        Console.WriteLine("{0}", answer1);
+        Console.WriteLine("{0}", answer3);
         // end
 
         return (answer1, answer3);
@@ -232,7 +346,22 @@ public class Program
         double answerLength = double.MaxValue;
 
         // code here
+        for (int i = 0; i < n; i++)
+        {
+            string result = Console.ReadLine();
+            string result2 = Console.ReadLine();
+            double x = double.Parse(result, new CultureInfo("en-US"));
+            double y = double.Parse(result2, new CultureInfo("en-US"));
 
+            double foo = Math.Sqrt(x * x + y * y);
+            if (foo < answerLength)
+            {
+                answerLength = foo;
+                answer = i + 1;
+            }
+        }
+        Console.WriteLine("{0}", answer);
+        Console.WriteLine("{0}", Math.Round(answerLength, 2));
         // end
 
         return (answer, answerLength);
@@ -242,7 +371,17 @@ public class Program
         double answer = double.MaxValue;
 
         // code here
+        for (int i = 0; i < n; i++)
+        {
+            string result = Console.ReadLine();
+            double foo = double.Parse(result, new CultureInfo("en-US"));
 
+            if (foo < answer)
+            {
+                answer = foo;
+            }
+        }
+        Console.WriteLine("{0}", answer);
         // end
 
         return answer;
@@ -252,7 +391,23 @@ public class Program
         int answer = 0;
 
         // code here;
+        for (int i = 0; i < n; i++)
+        {
+            string result1 = Console.ReadLine();
+            string result2 = Console.ReadLine();
+            string result3 = Console.ReadLine();
+            string result4 = Console.ReadLine();
+            double mark1 = double.Parse(result1, new CultureInfo("en-US"));
+            double mark2 = double.Parse(result2, new CultureInfo("en-US"));
+            double mark3 = double.Parse(result3, new CultureInfo("en-US"));
+            double mark4 = double.Parse(result4, new CultureInfo("en-US"));
 
+            if (mark1 != 2 && mark1 != 3 && mark2 != 2 && mark2 != 3 && mark3 != 2 && mark3 != 3 && mark4 != 2 && mark4 != 3)
+            {
+                answer++;
+            }
+        }
+        Console.WriteLine("{0}", answer);
         // end
 
         return answer;
@@ -263,7 +418,28 @@ public class Program
         double avg = 0.0;
 
         // code here;
+        for (int i = 0; i < n; i++)
+        {
+            string result1 = Console.ReadLine();
+            string result2 = Console.ReadLine();
+            string result3 = Console.ReadLine();
+            string result4 = Console.ReadLine();
+            double mark1 = double.Parse(result1, new CultureInfo("en-US"));
+            double mark2 = double.Parse(result2, new CultureInfo("en-US"));
+            double mark3 = double.Parse(result3, new CultureInfo("en-US"));
+            double mark4 = double.Parse(result4, new CultureInfo("en-US"));
 
+            if (mark1 == 2 || mark2 == 2 || mark3 == 2 || mark4 == 2)
+            {
+                answer++;
+            }
+            
+            avg += mark1 + mark2 + mark3 + mark4;
+        }
+
+        avg /= 4 * n;
+        Console.WriteLine("{0}", answer);
+        Console.WriteLine("{0}", avg);
         // end
 
         return (answer, avg);
@@ -273,7 +449,14 @@ public class Program
         double answer = 0;
 
         // code here;
-
+        if (r < 0) answer = 0;
+        else
+        {
+            if (type == 0) answer = Math.Round(r * r, 2);
+            else if (type == 1) answer = Math.Round(Math.PI * r * r, 2);
+            else if (type == 2) answer = Math.Round(r * r * Math.Sqrt(3) / 4, 2);
+        }
+        Console.WriteLine(answer);
         // end
 
         return answer;
@@ -283,7 +466,26 @@ public class Program
         double answer = 0;
 
         // code here;
+        if (A >= 0 && B >= 0)
+        {
+            if (type == 0) answer = Math.Round(A * B, 2);
+            else if (type == 1) answer = Math.Round(Math.PI * Math.Abs((Math.Pow(A, 2) - Math.Pow(B, 2))), 2);
+            else if (type == 2)
+            {
+                if (A >= B)
+                {
+                    double height = Math.Sqrt(A * A - (B * B) / 4); 
+                    answer = Math.Round(0.5 * B * height);   
+                }
+                else
+                {
+                    double height = Math.Sqrt(B * B - (A * A) / 4); 
+                    answer = Math.Round(0.5 * A * height, 2);     
+                }
+            }
+        }
 
+        Console.WriteLine(answer);
         // end
 
         return answer;
